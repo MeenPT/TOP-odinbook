f6b707ab12fd2d6947d5129103282f3c6651b5bd
Pakapol Tanachaisith <meen260600@gmail.com>
2025-01-25 10:22:42 +0700

    Install and config Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   root "posts#index"
   resources :posts
   get "up" => "rails/health#show", as: :rails_health_check
