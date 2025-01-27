7b23c336fda11dbc7e602f81fee74d04a4ad5083
Pakapol Tanachaisith <meen260600@gmail.com>
2025-01-27 21:06:42 +0700

    Refactir the sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def dashboard_sidebar_items
+    [
+     {
+      label: "Home",
+      path: root_path,
+      icon: "icons/home.svg"
+     },
+     {
+      label: "New post",
+      path: new_post_path,
+      icon: "icons/plus.svg"
+     }
+    ]
+  end
 end
