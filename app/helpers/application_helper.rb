4783f675a3c2001af7fd14864f2bbcc707428340
Pakapol Tanachaisith <meen260600@gmail.com>
2025-02-16 20:56:57 +0700

    Add form feedback to the signup form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def field_error(object, attribute)
+    return unless object&.errors&.any? && object.errors[attribute].present?
+
+    errors = object.errors.full_messages_for(attribute).join(", ")
+    content_tag(:div, errors, class: "form__field-error", role: "alert", aria: { live: "assertive" })
+  end
 end
