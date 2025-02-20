3b19d9ba3282a55fc01f395d023208bb27fc3393
Pakapol Tanachaisith <meen260600@gmail.com>
2025-02-20 12:01:30 +0700

    Build posts feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all.includes(:author)
+    @posts = Post.all.includes(:author).order(created_at: :desc)
   end
 
   def show
